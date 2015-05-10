a4062c86970b9723fdebc29760ef5dcf08c7f6ac
Patrick Allen <prallen90@gmail.com>
2015-05-10 12:32:20 -0400

    Fix circular imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from app.admin.views import admin
-from app.blog.views import blog
-from app.portfolio.views import portfolio
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../patallendb.db'
 db = SQLAlchemy(app)
 
+# Import Blueprints 
+from app.admin.views import admin
+from app.blog.views import blog
+from app.portfolio.views import portfolio
 # Register blueprints
 app.register_blueprint(admin)
 app.register_blueprint(blog)
 app.register_blueprint(portfolio)
 
+
 from app import views
+from app.blog import views
