d08ef6882501d04a6fed76881a34e3f98e61f9ec
Patrick Allen <prallen90@gmail.com>
2015-05-10 12:44:10 -0400

    Add blog home page functionality

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,12 +1,14 @@
 from flask import Blueprint
 from flask import render_template
+from app.models import Post
 
 blog = Blueprint('blog', __name__, url_prefix='/blog')
 
 
 @blog.route('/')
 def home():
-    return render_template('blog/home.html')
+    posts = Post.query.all()
+    return render_template('blog/home.html', posts=posts)
 
 
 @blog.route('/<int:post_id>')
