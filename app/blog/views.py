d78315366925afa5cc67386fdcdca7129cfb3ffe
Patrick Allen <prallen90@gmail.com>
2015-05-10 15:43:10 -0400

    Add pagination to blog home

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,16 +1,36 @@
 from flask import Blueprint
+from flask import abort
 from flask import render_template
 from app.models import Post
+from app.helpers import Pagination
 
 blog = Blueprint('blog', __name__, url_prefix='/blog')
 
-
-@blog.route('/')
-def home():
-    posts = Post.query.all()
-    return render_template('blog/home.html', posts=posts)
+POSTS_PER_PAGE = 5
 
 
-@blog.route('/<int:post_id>')
+def getPostsForPage(page, posts_per_page):
+    return Post.query.offset(posts_per_page * page - posts_per_page)\
+            .limit(posts_per_page)
+
+
+def getNumPosts():
+    return Post.query.count()
+
+
+@blog.route('/', defaults={'page': 1})
+@blog.route('/page/<int:page>/')
+def home(page=1):
+    count = getNumPosts()
+    posts = getPostsForPage(page, POSTS_PER_PAGE)
+    pagination = Pagination(page, 11, POSTS_PER_PAGE)
+    return render_template('blog/home.html', posts=posts, pagination=pagination)
+
+
+@blog.route('/post/<int:post_id>')
 def post(post_id):
-    return "This is the blog post {}".format(post_id)
+    try:
+        post = Post.query.get(post_id)
+    except:
+        abort(404)
+    return render_template('blog/post.html', post=post)
--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,23 @@
 import re
+from math import ceil
+
+class Pagination():
+    def hasPrev(self):
+        if self.page > 1:
+            return True
+        else:
+            return False
+
+    def hasNext(self):
+        if self.page < self.numPages:
+            return True
+        else:
+            return False
+
+    def __init__(self, page, count, items_per_page):
+        self.page = page
+        self.numPages = ceil(count / items_per_page)
+
 
 def getExcerpt(html, length):
     """Takes html, strips it, and returns truncated text based on length"""
