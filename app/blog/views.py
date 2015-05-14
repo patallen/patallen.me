959ad8a7ccf65fa3457c31e574a09007373d6d8a
Patrick Allen <prallen90@gmail.com>
2015-05-14 15:14:52 -0400

    Add user authentication for add/edit blog posts

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,13 +1,9 @@
-from flask import Blueprint
-from flask import abort
-from flask import redirect 
-from flask import url_for 
-from flask import render_template
+from flask import abort, Blueprint, redirect, render_template, url_for
 from app.models import Post
 from app.helpers import Pagination
 from app import db
 from .forms import PostForm
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 blog = Blueprint('blog', __name__, url_prefix='/blog')
 
@@ -51,7 +47,7 @@ def addPost():
     
     if form.validate_on_submit():
         post = Post()
-        post.author = 1
+        post.author = current_user.id
         post.title = form.title.data
         post.body_md = form.body.data
         db.session.add(post)
@@ -67,9 +63,10 @@ def addPost():
 def editPost(post_id):
     """Edit an existing blog post"""
     post = Post.query.get(post_id)
+    # Check that user is the owner of the project (not necessary atm)
+    if current_user.id != post.author:
+        return "You do not have permission to edit this blog post."
     form = PostForm()
-    form.title.data = post.title
-    form.body.data = post.body_md
 
     if form.validate_on_submit():
         post.title = form.title.data
@@ -77,5 +74,9 @@ def editPost(post_id):
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('blog.post', post_id=post.id))
+
+    # Pre-populate form with existing data
+    form.title.data = post.title
+    form.body.data = post.body_md
 
     return render_template('blog/edit.html', form=form, post=post)
