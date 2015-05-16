80c43add8b98b9ea665efa813c94e8f1ec9a9a72
Patrick Allen <prallen90@gmail.com>
2015-05-16 12:32:04 -0400

    Configure app from environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_envvar('PATALLENME_SETTINGS')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
