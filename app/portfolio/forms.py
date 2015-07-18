ed682faba52ec8e657d7807da34ac7db0bd61d34
Patrick Allen <prallen90@gmail.com>
2015-07-17 23:00:40 -0400

    Make live_url and github_url optional

--- app/portfolio/forms.py
+++ app/portfolio/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Optional
 
 
 class ProjectForm(Form):
     """WTForm for creating/editing portfolio project"""
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    blog_slug = StringField('Blog Slug')
+    blog_slug = StringField('Blog Slug', validators=[Optional()])
     stack = StringField('Stack')
-    github_url = StringField('Github URL', validators=[URL()])
-    live_url = StringField('Live URL', validators=[URL()])
+    github_url = StringField('Github URL', validators=[Optional(), URL()])
+    live_url = StringField('Live URL', validators=[Optional(), URL()])
     order_num = IntegerField('Order Number')
     image_url = StringField('Image URL', validators=[URL()])
