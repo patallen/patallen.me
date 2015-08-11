9b025e6aa1867306af49e1eadf2e268ba8bdc765
Patrick Allen <prallen90@gmail.com>
2015-08-11 12:27:11 -0400

    Remove temporary choices from blog_post field in form

--- app/portfolio/forms.py
+++ app/portfolio/forms.py
@@ -7,7 +7,7 @@ class ProjectForm(Form):
     """WTForm for creating/editing portfolio project"""
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    blog_post = SelectField('Blog Post', validators=[Optional()], choices=['12', 'kj'])
+    blog_post = SelectField('Blog Post', validators=[Optional()])
     stack = StringField('Stack')
     github_url = StringField('Github URL', validators=[Optional(), URL()])
     live_url = StringField('Live URL', validators=[Optional(), URL()])
