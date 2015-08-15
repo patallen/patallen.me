d035c9200db9679b0d4461aacffe8f10a4b6eb36
Patrick Allen <prallen90@gmail.com>
2015-08-15 16:05:22 -0400

    Add published BooleanField to PostForm

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -1,24 +1,28 @@
 from flask_wtf import Form
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired
 from app.models import Category
 
+
 def categories_factory():
     """
-    Return a queryset that contains all categories -- 
+    Return a queryset that contains all categories --
     used in PostForm QuerySelectField
     """
     return Category.query.all()
 
+
 class PostForm(Form):
-	"""WTForm for creating/editing blog post"""
-	title = StringField('Title', validators=[DataRequired()])
-	body_md = TextAreaField('Body', validators=[DataRequired()])
-	category = QuerySelectField('Category',
-	                            query_factory=categories_factory,
-	                            allow_blank=False)
+    """WTForm for creating/editing blog post"""
+    title = StringField('Title', validators=[DataRequired()])
+    body_md = TextAreaField('Body', validators=[DataRequired()])
+    published = BooleanField('Published')
+    category = QuerySelectField('Category',
+                                query_factory=categories_factory,
+                                allow_blank=False)
+
 
 class DeleteForm(Form):
-	"""WTForm for deleting a blog post"""
-	delete = SubmitField('DELETE')
+    """WTForm for deleting a blog post"""
+    delete = SubmitField('DELETE')
